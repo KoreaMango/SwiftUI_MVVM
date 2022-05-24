7ee1d84f5ea8dbcc38c17f175ca946c17a75cc83
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-05-24 22:51:02 +0900

    Initial Commit

---
+++ MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by 강민규 on 2022/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
